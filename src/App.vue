bea6aa2bfd6542104055f9e1ad1a292be57cc165
wuchangwei <985215993@qq.com>
2021-06-23 20:31:15 +0800

    引入iconfont

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="docker">
+    <span class="docker__item">
+      <div class="iconfont">&#xe9db;</div>
+      首页
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe638;</div>
+      购物车
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe60e;</div>
+      订单
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe660;</div>
+      我的
+    </span>
   </div>
 </template>
 
 <style lang="scss">
+  .docker {
+    display: flex;
+    position: absolute;
+    box-sizing: border-box;
+    padding: 0 .18rem;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 0.49rem;
+    border-top: 1px solid #F1F1F1;
+  }
+
+  .docker__item {
+    flex: 1;
+    text-align: center;
+  }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import "normalize.css";
 import "./style/base.scss";
-
+import "./style/iconfont.css";
 createApp(App)
   .use(store)
   .use(router)
